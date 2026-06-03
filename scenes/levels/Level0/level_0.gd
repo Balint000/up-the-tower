3808344bebd532988f936a7a6ff755ed4611ea69
Balint000 <erdeibalint01@gmail.com>
2026-06-03 21:29:26 +0200

    Level0.8

--- scenes/levels/Level0/level_0.gd
+++ scenes/levels/Level0/level_0.gd
@@ -1,11 +1,15 @@
 extends Node2D
 
 @onready var health_bar = $Hud/CanvasLayer/VBoxContainer/HealthBar
+@onready var tutorial_sign_2: Area2D = %TutorialSign2
+@onready var tutorial_sign_1: Area2D = %TutorialSign1
+@onready var tutorial_sign_3: Area2D = $Signs/TutorialSign3
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
-
+	tutorial_sign_2._set_message("W or space to jump.")
+	tutorial_sign_1._set_message("A and D or arrow keys to move.")
+	tutorial_sign_3._set_message("Z for attack; shift for ability")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
